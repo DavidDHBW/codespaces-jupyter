85cb39a5a2efef22d26aedb88b6f7119f2ab2184
DavidDHBW <152873713+DavidDHBW@users.noreply.github.com>
2023-12-08 14:41:13 +0000

    hinzufügen von Code

--- Python/hangman.py
+++ Python/hangman.py
@@ -1,6 +1,30 @@
+def NutzerEingabe():
+    inputChar = input()
+    if inputChar == solution:
+        gameOver == True
+        won = True
+    else:
+        for i in range(0, len(solution)):
+            if inputChar[0] == solution[i]:
+                print("Correct '" + inputChar[0] + "' is in the word")
+        print(inputChar[0])
+
+        
+
 #var
+gameOver = False
 solution = "test" #hier kann später eine Liste mit versch. Wörtern verwendet werden um zufälliges wort auszuwählen
-for x in range(0, solution.len):
-    output = ""
+output = ""
+for i in range(0, len(solution)):
+    output = output + "*"
+print(output)
+
+while gameOver == False:
+    print("Rate einen Buchstaben...")
+    NutzerEingabe()
+
+
+       
+
 
 
