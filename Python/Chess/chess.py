8e727d2c925a98c55b5c3d15f712c76e28196cd7
DavidDHBW <152873713+DavidDHBW@users.noreply.github.com>
2023-12-22 10:48:29 +0000

    start of a chess game

---
+++ Python/Chess/chess.py
@@ -0,0 +1,11 @@
+#initialize playing board, 8X8 array
+#w = white, b = black, r= rock, n = knight, b = bishop, q= queen, k = king, p= pawn, --= empty field
+board = [["br","bn","bb","bq","bk","bb","bn","br"],
+         ["bp","bp","bp","bp","bp","bp","bp","bp"],
+         ["--","--","--","--","--","--","--","--"],
+         ["--","--","--","--","--","--","--","--"],
+         ["--","--","--","--","--","--","--","--"],
+         ["--","--","--","--","--","--","--","--"],
+         ["wp","wp","wp","wp","wp","wp","wp","wp"],
+         ["wr","wn","wb","wq","wk","wb","wn","wr"]
+         ]
