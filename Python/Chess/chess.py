17a96f8fcb723b19891e41e7db81dafb13b8f9e3
DavidDHBW <152873713+DavidDHBW@users.noreply.github.com>
2024-01-24 15:18:34 +0000

    added png images for the figurs and created an loading function for them. Also added some settings for the gui

--- Python/Chess/chess.py
+++ Python/Chess/chess.py
@@ -3,15 +3,15 @@ class gameState():
     def __init__(self):
         
         #initialize playing board, 8X8 array
-        #w = white, b = black, r= rock, n = knight, b = bishop, q= queen, k = king, p= pawn, --= empty field
-        self.board = [["br","bn","bb","bq","bk","bb","bn","br"],
+        #w = white, b = black, R= rock, N = knight, B = bishop, Q= queen, K = king, p= pawn, --= empty field
+        self.board = [["bR","bN","bB","bQ","bK","bB","bN","bR"],
                 ["bp","bp","bp","bp","bp","bp","bp","bp"],
                 ["--","--","--","--","--","--","--","--"],
                 ["--","--","--","--","--","--","--","--"],
                 ["--","--","--","--","--","--","--","--"],
                 ["--","--","--","--","--","--","--","--"],
                 ["wp","wp","wp","wp","wp","wp","wp","wp"],
-                ["wr","wn","wb","wq","wk","wb","wn","wr"]
+                ["wR","wN","wB","wQ","wK","wB","wN","wR"]
                 ]
         self.whiteToMove = True
         
--- Python/Chess/chessMain.py
+++ Python/Chess/chessMain.py
@@ -1,1 +1,20 @@
-#inputs output file 
+#inputs output file
+import pygame as p
+from Chess import chess
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8 #chess board is 8x8
+SQUARE_SIZE = WIDTH/DIMENSION
+MAX_FPS = 15 #if I want to add animations later
+IMAGES ={}
+
+def loadImages():
+    pieces = ['wp','wR','wN','wB','wQ','wK','bp','bR','bN','bB','bQ','bK']
+    for piece in pieces:
+        IMAGES[piece] = p.transform(p.image.load("images/" + piece + ".png"),[SQUARE_SIZE,SQUARE_SIZE])   
+#get the image for the white pawn by 'IMAGES['wp']'    
+#to adjust the image size to the square size i used p.transform()
+
+
+        
+
