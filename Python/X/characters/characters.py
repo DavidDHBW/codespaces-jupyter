95c37d4d8429c917133ff354ff8583ecbdeeee61
DavidDHBW <152873713+DavidDHBW@users.noreply.github.com>
2024-02-14 17:41:02 +0100

    damge caculation fertig gemacht
    fein kallibrierung der Werte evtl noch notwendig.
    
    sword-> axe-> spear -> sword
    gibt advantage oder disadvantage.
    sowie der damage und defense wert einer waffe von dem angreifenden und abwehrenden gegner

--- Python/X/characters/characters.py
+++ Python/X/characters/characters.py
@@ -1,25 +1,29 @@
-global selectedEnemyCharacters
-selectedPlayerCharacters = [3]
-selectedEnemyCharacters = [3]
+from damageCalculation import DamageCalculation
+from weapon import Weapon
+
 
 class Character:
-    def __init__(self, name, health,enemy, level, warriorType, weapon, hasMove, armor):
+    def __init__(self, name, health,enemy, level, warriorType, weaponType, hasMove, armor):
         self.name = name #display name
         self.health = health 
         self.enemy = enemy #bool 
         self.level = level # from 1-10
         self.warriorType = warriorType #wizzard , rangedFighter, meleeFighter
-        self.weapon = weapon #depends also on the worrior type
+        self.weapon = Weapon(weaponType) #depends also on the worrior type
         self.hasMove = hasMove
         self.armor = armor #from 1-5
     
-    def damageCalculation(self, target):
-        pass
+
            
     def getDamage(self,damage):
         self.health -= damage
         
     def attack(self, target):
-        self.damageCalculation( target)
+        dc = DamageCalculation(self, target)
+        damage = dc.damage
+        print(damage)
         
     
+char = Character("char", 50,False,4,"meleeFighter", "spear",True,10)
+target = Character("char", 50,False,4,"meleeFighter", "sword",True,10)
+char.attack(target)
--- Python/X/characters/damageCalculation.py
+++ Python/X/characters/damageCalculation.py
@@ -1,15 +1,33 @@
 import random
 import characterLists
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from rangedInteger import RangedInt 
 enemies = characterLists.selectedEnemyCharacters
 
+advantages = []
+disadvantages = []
 class DamageCalculation:
-    def DamageCalculation(self, character, target):
-        ranLow = 0.8
-        globals
-        ranFactor = random.uniform(ranLow,1.1)
-        ranFactorDif -= self.onFieldEnemy(target)
-        ranFactorDif += self.weaponAdv(character, target)
+    def __init__(self, character, target):
+        self.ranFactorDif = self.getRanFactorDiff(character, target)
+        self.ranFactor = self.getRanFactor()
+        self.damage =  int(character.weapon.damage * self.ranFactor)
+
         
+    def getRanFactor(self):
+        ranLow = 0.8
+        ranHigh = 1.2
+        ranLow += self.ranFactorDif
+        ranHigh += self.ranFactorDif
+        ranFactor = random.uniform(ranLow,ranHigh)
+        return ranFactor    
+    def getRanFactorDiff(self, character, target):
+        ranFactorDiff = 0.0
+        ranFactorDiff -= self.onFieldEnemy(target)
+        ranFactorDiff -= self.weaponAdv(character, target)
+        ranFactorDiff -= self.weaponDeffenseDiff(target)
+        return ranFactorDiff    
     def onFieldEnemy(self, target): # calc disatvantage of the enemies on field (if melee fighter is there wizzard is protectet)
 
         if target.warriorType == "meleeFighter":
@@ -35,9 +53,26 @@ class DamageCalculation:
                     return 0.5
         else: return 0
         
-            
-    
         #wizzard , rangedFighter, meleeFighter
-    def weaponAdv(self, characte, target):
-        pass
+    def weaponAdv(self, character, target):
+        #character is meleeFighter
+        if character.warriorType ==  "meleeFighter":
+            if target.warriorType == "meleeFighter":
+                weapons = ["sword","axe","spear"]
+                for t in range(0,3):
+                    i = RangedInt(startValue=t,minValue=0,maxValue=2) # self createt limited int to have the triangle advantage/disadvantage
+                    if character.weapon.weaponType == weapons[i.number]:
+                        if target.weapon.weaponType ==weapons[i.number]:
+                            return 0
+                        if target.weapon.weaponType == weapons[i.raiseInt()]:
+                            return -0.2
+                        if target.weapon.weaponType == weapons[i.raiseInt()]:
+                            return 0.2
+        #character is rangedFighter || no impact
+        #character is wizzard || no impact
+    def weaponDeffenseDiff(self,target):
+        defense = target.weapon.defense
+        return defense/25 # bei 5 defense Punkten disatvantage von 0.2
         
+  
+ 
